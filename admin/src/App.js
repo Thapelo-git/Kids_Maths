b5d77c963b1193ba2be5726501c238bb1c095071
Thapelo-git <chabathapelo1@gmail.com>
2022-08-17 20:29:50 +0200

    commit

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 //https://www.youtube.com/watch?v=6euUmHWOH9g
 const App = () => {
+  const commands=[
+    {
+      command:'reset',
+      callback:({resetTranscript})=>resetTranscript()
+    },   {
+      command:'colour *',
+      callback:(color)=>{
+        document.body.style.background=color;
+      }
+    }, {
+      command:'alphabet *',
+      callback:(alphabet)=>{
+        document.documentElement=alphabet;
+      }
+    }
+  ]
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+  } = useSpeechRecognition({commands});
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
@@ -16,7 +32,7 @@ const App = () => {
   return (
     <div>
       {/* <p>Microphone: {listening ? 'on' : 'off'}</p> */}
-      <button onClick={SpeechRecognition.startListening({ continuous: true,language:'en-sotho' })}>Start</button>
+      <button onClick={SpeechRecognition.startListening({ continuous: true, })}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
       <p>{transcript}</p>
